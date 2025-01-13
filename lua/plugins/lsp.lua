bf05f6d6607ba888fd074ac0fcd2104093385a2c
Sibiraj <20282546+sibiraj-s@users.noreply.github.com>
2025-01-13 11:18:51 +0530

    Bump dependencies

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -11,7 +11,7 @@ return {
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
-          "tsserver",
+          "ts_ls",
         },
       })
     end,
@@ -21,7 +21,7 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+      lspconfig.ts_ls.setup({})
     end,
   },
 }
